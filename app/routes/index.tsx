55bbf4c48486fc6ef2d4b8491b5e99594a2ce193
sri venkatesh lp <lps.162@gmail.com>
2022-02-11 20:35:00 +0530

    Remix app on Flare

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Musicality</h1>
-      <h3>React, Remix on Cloudflare Pages</h3>
+      <h3>React, Remix, JAM Stack, Cloudflare</h3>
     </div>
   );
 }
